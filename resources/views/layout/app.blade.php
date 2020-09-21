2ee090f9f07790f8da4925d4178b823bd6368e17
scratchuz4k <whiteknight96@hotmail.com>
2020-09-21 17:51:30 +0100

    Remake

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,45 +1,60 @@
-<!--
- * A Design by GraphBerry
- * Author: GraphBerry
- * Author URL: http://graphberry.com
- * License: http://graphberry.com/pages/license
--->
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Leiricanal</title>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Load fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
-
-    <!--Load styles -->
-    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/animate.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    position: relative; 
+  }
+  #home {
+      padding-top:50px;height:100vh;
+      background-image: url("{{ asset('img/Slider.jpg') }}");
+      background-repeat: no-repeat;
+      background-size: cover;
+      }
+  #section2 {padding-top:50px;height:500px;color: #fff; background-color: #673ab7;}
+  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #ff9800;}
+  #section41 {padding-top:50px;height:500px;color: #fff; background-color: #00bcd4;}
+  #section42 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
+  </style>
 </head>
 <body>
+
 @yield('content')
+
+<script>
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
+</script>
+
 </body>
-
-<!-- Load jQuery -->
-<script type="text/javascript" src="{{asset('js/jquery-1.11.2.min.js')}}"></script>
-
-<!-- Load Booststrap -->
-<script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
-
-<!-- Load custom js plugins -->
-<script type="text/javascript" src="{{asset('js/jquery.mixitup.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/wow.js')}}"></script>
-<script type="text/javascript" src="{{asset('js/smooth-scroll.js')}}"></script>
-
-<!-- Load custom js for theme -->
-<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 </html>
