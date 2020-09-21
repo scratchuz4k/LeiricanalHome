ee55a7ea3a5600a06ff08f112db8f26fe6801ca5
SCRaTCHuZ4K <whiteknight96@hotmail.com>
2020-09-21 12:53:29 +0100

    Initial commit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,45 @@
+<!--
+ * A Design by GraphBerry
+ * Author: GraphBerry
+ * Author URL: http://graphberry.com
+ * License: http://graphberry.com/pages/license
+-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leiricanal</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Load fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+
+    <!--Load styles -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/animate.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+</head>
+<body>
+@yield('content')
+</body>
+
+<!-- Load jQuery -->
+<script type="text/javascript" src="{{asset('js/jquery-1.11.2.min.js')}}"></script>
+
+<!-- Load Booststrap -->
+<script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
+
+<!-- Load custom js plugins -->
+<script type="text/javascript" src="{{asset('js/jquery.mixitup.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/wow.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/smooth-scroll.js')}}"></script>
+
+<!-- Load custom js for theme -->
+<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+</html>
