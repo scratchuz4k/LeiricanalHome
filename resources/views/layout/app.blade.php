20eaff9a9e2e8cfea770213e21d56f530e2c881a
scratchuz4k <whiteknight96@hotmail.com>
2020-09-22 18:29:59 +0100

    wtf am i doing

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,10 +4,46 @@
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
   <style>
+
+  .navbar{
+    background-color:white;
+    border-bottom: 1px solid #1f1475;
+    width: 100vw;
+    padding: 0;
+    position:fixed;
+    z-index: 1;
+  }
+  
+  .nav-link {
+    color: #1f1475;
+    text-align: center;
+  }
+
+  .nav-link:hover{
+    color: white;
+    background-color: #1f1475;
+    transition: 0.5s;
+  }
+  
+  
+    color: white;
+  }
+
+  .navbar-brand{
+    padding: 1vh;
+  }
+
+
+  .navbar-brand img{
+    width:40%;
+    height:100%;
+    padding: 1px;
+  }
+  
+
   body {
     position: relative; 
   }
@@ -16,12 +52,17 @@
       background-image: url("{{ asset('img/Slider.jpg') }}");
       background-repeat: no-repeat;
       background-size: cover;
+      color:white;
+      font-weight: bold;
       }
-  #section2 {padding-top:50px;height:500px;color: #fff; background-color: #673ab7;}
-  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #ff9800;}
-  #section41 {padding-top:50px;height:500px;color: #fff; background-color: #00bcd4;}
-  #section42 {padding-top:50px;height:500px;color: #fff; background-color: #009688;}
+
+  #section2 {padding-top:8vh;height:500px;color: #fff; background-color: #673ab7;}
+  #section3 {padding-top:8vh;height:500px;color: #fff; background-color: #ff9800;}
+  #section41 {padding-top:8vh;height:500px;color: #fff; background-color: #00bcd4;}
+  #section42 {padding-top:8vh;height:500px;color: #fff; background-color: #009688;}
   </style>
+
+  
 </head>
 <body>
 
@@ -56,5 +97,9 @@ $(document).ready(function(){
 });
 </script>
 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+ 
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,32 +2,43 @@
 
 
 @section('content')
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li><a href="#home">Section 1</a></li>
-          <li><a href="#section2">Section 2</a></li>
-          <li><a href="#section3">Section 3</a></li>
-          <li><a href="#">Section 4</a></li>
-        </ul>
-      </div>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Quem Somos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Produtos e Serviços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Distinções</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Portfólio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+
+    </ul>
   </div>
-</nav>    
+</nav>
 
 <div id="home" class="container-fluid">
-  <h1>Section 1</h1>
-  <p>Click on the different Section links in the navbar to see the smooth scrolling effect.</p>
+  <div class="row justify-content-md-center align-items-center" style="min-height:100%;">
+    <div class="col-md-auto">
+      One of three columns
+    </div>
+  </div>
 </div>
 
 
@@ -37,17 +48,22 @@
   <h1>Section 2</h1>
   <p>Click on the different Section links in the navbar to see the smooth scrolling effect.</p>
 </div>
+
+
 <div id="section3" class="container-fluid">
   <h1>Section 3</h1>
   <p>Click on the different Section links in the navbar to see the smooth scrolling effect.</p>
 </div>
-<div id="section41" class="container-fluid">
-  <h1>Section 4 Submenu 1</h1>
+<div id="section4" class="container-fluid">
+  <h1>Section 4</h1>
   <p>Click on the different Section links in the navbar to see the smooth scrolling effect.</p>
 </div>
-<div id="section42" class="container-fluid">
-  <h1>Section 4 Submenu 2</h1>
+<div id="section5" class="container-fluid">
+  <h1>Section 5</h1>
+  <p>Click on the different Section links in the navbar to see the smooth scrolling effect.</p>
+</div>
+<div id="section6" class="container-fluid">
+  <h1>Section 5</h1>
   <p>Click on the different Section links in the navbar to see the smooth scrolling effect.</p>
 </div>
 @endsection
-
